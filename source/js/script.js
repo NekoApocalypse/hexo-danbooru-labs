e73f4511a1961e8345de885e16396831768c08e2
NekoApocalypse <xiaoqiang01npc@gmail.com>
2018-04-22 11:37:13 +0800

    JS Fade Action Fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 let menu_show = false;
 
-$(document).on('pagebeforeshow', function(){
+$(window).on('unload', function() {
+  console.log('unload');
   $(document).find('.highlight').removeClass('highlight');
   $(document).find('.clicked').removeClass('clicked');
   $(document).find('.float-menu li').hide();
@@ -8,42 +9,43 @@ $(document).on('pagebeforeshow', function(){
 });
 
 $(document).ready(function() {
-  console.log('fading');
+  console.log('ready');
+
   $('.fade-in').fadeIn('slow');
   $('.fade-in-seq').each(function(i) {
     $(this).delay(50 * i).fadeIn('slow');
   });
-});
 
-$('.article-card').hover(function() {
-  $(this).find('.article-title').toggleClass('highlight');
-});
+  $('.article-card').hover(function() {
+    $(this).find('.article-title').toggleClass('highlight');
+  });
 
-$('.article-card').click(function() {
-  $(this).find('.article-title').addClass('highlight');
-});
+  $('.article-card').click(function() {
+    $(this).find('.article-title').addClass('highlight');
+  });
 
-$('.burger-icon').click(function() {
-  $(this).toggleClass('clicked');
-  if(!menu_show) {
-    menu_show = true;
-    $('.float-menu li').each(function(i) {
-      $(this).delay(50 * i).fadeIn(500);
-    });
-  } else {
-    menu_show = false;
-    $('.float-menu li').each(function(i){
-      $(this).delay(50 * i).fadeOut(500);
-    });
-  }
-});
-
-$(document).click(function(e) {
-  if ($(e.target).closest('.burger-icon').length === 0) {
-      $('.burger-icon').removeClass('clicked');
+  $('.burger-icon').click(function() {
+    $(this).toggleClass('clicked');
+    if(!menu_show) {
+      menu_show = true;
+      $('.float-menu li').each(function(i) {
+        $(this).delay(50 * i).fadeIn(500);
+      });
+    } else {
       menu_show = false;
       $('.float-menu li').each(function(i){
         $(this).delay(50 * i).fadeOut(500);
       });
     }
+  });
+
+  $(document).click(function(e) {
+    if ($(e.target).closest('.burger-icon').length === 0) {
+        $('.burger-icon').removeClass('clicked');
+        menu_show = false;
+        $('.float-menu li').each(function(i){
+          $(this).delay(50 * i).fadeOut(500);
+        });
+      }
+  });
 });
