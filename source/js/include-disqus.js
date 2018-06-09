f4ea69df8e7220c91b04afeb564971d245f9591f
NekoApocalypse <xiaoqiang01npc@gmail.com>
2018-06-09 15:15:20 +0800

    Added Disqus

--- source/js/include-disqus.js
+++ source/js/include-disqus.js
@@ -1,3 +1,17 @@
-var disqus_config=function(){
-    this.page.url='';
-}
+console.log('JS loaded!')
+var post_url = $('#post-url').text()
+console.log(post_url)
+var post_identifier = $('#post-identifier').text()
+console.log(post_identifier);
+
+var disqus_config = function () {
+this.page.url = post_url;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = post_identifier; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://danbooru-labs-moe.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
